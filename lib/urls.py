870fb2bf11095c5acb3f2812c21a14777b4d161e
zhonglunsheng <873946453@qq.com>
2019-11-17 17:01:37 +0800

    feat:完成MTV增删查改

--- lib/urls.py
+++ lib/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
-
+app_name = 'lib'
 urlpatterns = [
     path('', views.index, name='index'),
     path('detail/', views.detail, name='detail'),
+    path('addBook/', views.addBook, name='addBook'),
+    path('delBook/<int:book_id>', views.delBook, name='delBook'),
+
 ]
 
--- lib/views.py
+++ lib/views.py
@@ -1,4 +1,5 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from django.shortcuts import render
 from .models import Book
 # Create your views here.
@@ -12,3 +13,21 @@ def detail(request):
     book_list = Book.objects.order_by('-pub_date')[:5]
     context = {'book_list': book_list}
     return render(request, 'detail.html', context)
+
+
+def addBook(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        author = request.POST['author']
+        pub_house = request.POST['pub_house']
+
+    from django.utils import timezone
+    book = Book(name=name, author=author, pub_house=pub_house, pub_date=timezone.now())
+    book.save()
+
+    return HttpResponseRedirect(reverse('lib:detail'))
+
+
+def delBook(request, book_id):
+    Book.objects.filter(id=book_id).delete()
+    return HttpResponseRedirect(reverse('lib:detail'))
